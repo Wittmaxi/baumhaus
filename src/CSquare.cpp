6e1ff9728052b4ca4bf4d8c90b829e8946ebc3d9
PhiluminaEngine <wittmaxi@outlook.de>
2017-04-05 14:58:01 +0200

    Squares

---
+++ src/CSquare.cpp
@@ -0,0 +1,11 @@
+#include "CSquare.h"
+
+CSquare::CSquare()
+{
+    //ctor
+}
+
+CSquare::~CSquare()
+{
+    //dtor
+}
---
+++ src/CSquare.h
@@ -0,0 +1,16 @@
+#ifndef CSQUARE_H
+#define CSQUARE_H
+
+
+class CSquare
+{
+    public:
+        CSquare();
+        virtual ~CSquare();
+
+    protected:
+
+    private:
+};
+
+#endif // CSQUARE_H
