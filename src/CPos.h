dc60b63ec9155bf3aa8628479709ce5fd80803f3
PhiluminaEngine <wittmaxi@outlook.de>
2017-05-29 19:14:00 +0200

    made the squares-array an array of pointers

--- src/CPos.h
+++ src/CPos.h
@@ -26,7 +26,7 @@ class CPos
         CSquare *getSquarePointer(int x, int y);
 
     protected:
-        CSquare squares[8][8]; //FIRST x, THEN y.
+        CSquare *squares[8][8]; //FIRST x, THEN y.
         std::string fen;
     private:
         void  parseFen(std::string fen);
